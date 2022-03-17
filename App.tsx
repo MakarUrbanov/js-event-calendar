b21f5d8187095c35a2f730b66eeeff73056e2a41
makar <mmk@liis.su>
2022-03-18 01:32:32 +0300

    add more libraries

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <View>
-      <Text>
-        Hello
-      </Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <View>
+          <Text>
+            Hello
+          </Text>
+        </View>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 };
 
